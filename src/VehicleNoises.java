cc522a38acba7b1745ff7e7ca39f98400164ec27
lscha <lscha@DESKTOP-6B90PBC>
2023-01-08 17:41:10 -0600

    noises

---
+++ src/VehicleNoises.java
@@ -0,0 +1,13 @@
+import model.Example
+
+public class VehicleNoises {
+	
+	// add your animal class to the model package
+	// only push this file and the animal class you created
+
+	public static void main(String[] args) {
+		Example example = new Example();
+		System.out.println(example.makeNoise());
+	}
+
+}
